ae8116c47469b26cbbdaacbd2e6ec31d012b66a6
roshan pujara <pujararoshan984@gmail.com>
2023-06-09 15:44:21 +0545

    feat:creating routes and sending json formatted data to the client

--- day 2 node/server.js
+++ day 2 node/server.js
@@ -2,7 +2,32 @@ import express from "express";
 import "dotenv/config";
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(201).json({
+    status: true,
+    message: "user is here",
+  });
+});
+app.get("/user", (req, res) => {
+  const users = ["ram", "laxman", "bharat"];
+  res.status(200).json({
+    status: true,
+    data: users,
+    message: "users added ",
+  });
+});
+app.get("/user/new", (req, res) => {
+  const users = ["ram", "laxman", "bharat"];
+  users.push("syam");
+  res.status(200).json({
+    status: true,
+    data: users,
+    message: "users added ",
+  });
+});
 app.listen(PORT, () => {
   console.log("hey there connected succesfully");
 });
